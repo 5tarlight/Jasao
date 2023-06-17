8766f71983f31f44c5af4b49456ff8f6ce4e2de2
YEAHx4 <developerstarlight@gmail.com>
2023-06-17 11:42:54 +0900

    User Entity

---
+++ jasao-server/src/main/kotlin/io/yeahx4/jasao/entity/User.kt
@@ -0,0 +1,31 @@
+package io.yeahx4.jasao.entity
+
+import io.yeahx4.jasao.role.UserRole
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+
+@Entity
+class User(
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
+
+    @Column(nullable = false, length = 100)
+    val email: String,
+
+    @Column(nullable = false, length = 30)
+    val username :String,
+
+    @Column(nullable = false, length = 255)
+    val password: String,
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    val role: UserRole
+) : TimeEntity()
