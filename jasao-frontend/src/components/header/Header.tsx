b5c27dc2936501f998ca4190b97134d2ddb9df08
YEAHx4 <developerstarlight@gmail.com>
2023-07-05 15:39:56 +0900

    Header logo and login detection

--- jasao-frontend/src/components/header/Header.tsx
+++ jasao-frontend/src/components/header/Header.tsx
@@ -1,20 +1,31 @@
 import styles from "../../styles/header/Header.module.scss";
 import classNames from "classnames/bind";
 import HeaderInput from "./HeaderInput";
+import HeaderLogo from "./HeaderLogo";
+import { useEffect, useState } from "react";
+import { getStorage } from "../../util/storage";
+import { Link } from "react-router-dom";
 
 const cx = classNames.bind(styles);
 
 export default function Header() {
+  const [login, setLogin] = useState(false);
+
+  useEffect(() => {
+    const storage = getStorage();
+    setLogin((storage && storage.user && storage.user.login) || false);
+  }, []);
+
   return (
     <header className={cx("header")}>
       <div className={cx("header-content")}>
         <div>
-          <h1>Jasao</h1>
+          <HeaderLogo />
         </div>
         <div>
           <HeaderInput />
         </div>
-        <div>Login</div>
+        {login ? <div>Welcome</div> : <Link to={"/login"}>로그인</Link>}
       </div>
     </header>
   );
