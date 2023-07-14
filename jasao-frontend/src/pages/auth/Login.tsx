7fcea74cf14f06951d5c29206a60fb95676e3513
YEAHx4 <developerstarlight@gmail.com>
2023-07-15 00:26:46 +0900

    Login and Signup Page

--- jasao-frontend/src/pages/auth/Login.tsx
+++ jasao-frontend/src/pages/auth/Login.tsx
@@ -1,13 +1,36 @@
-export default function Login() {
-  // useEffect(() => {
-  //   saveStorage({
-  //     user: {
-  //       jwt: "",
-  //       login: true,
-  //       refresh: "",
-  //     },
-  //   });
-  // }, []);
+import { Link } from "react-router-dom";
+import styles from "../../styles/auth/auth.module.scss";
+import classNames from "classnames/bind";
 
-  return <h1>Login</h1>;
+const cx = classNames.bind(styles);
+
+export default function Login() {
+  return (
+    <div className={cx("aligner")}>
+      <div className={cx("container")}>
+        <div className={cx("title-container")}>
+          <h2 className={cx("title")}>로그인</h2>
+          <nav className={cx("subtitle")}>Jasao 계정으로 로그인</nav>
+        </div>
+
+        <div className={cx("input-container")}>
+          <input className={cx("input")} placeholder="이메일" type="email" />
+          <input
+            className={cx("input")}
+            placeholder="비밀번호"
+            type="password"
+          />
+          <div className={cx("unable")}>
+            <Link to={"/auth/find"}>로그인할 수 없습니다.</Link>
+          </div>
+          <div className={cx("btn-container")}>
+            <button>로그인</button>
+          </div>
+        </div>
+        <div className={cx("menu-container")}>
+          <Link to={"/auth/signup"}>계정이 없습니다.</Link>
+        </div>
+      </div>
+    </div>
+  );
 }
