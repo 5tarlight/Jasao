1da29666dc43cad2234d089c62ce3b10c05b1c33
YEAHx4 <developerstarlight@gmail.com>
2023-07-26 01:36:49 +0900

    logout

--- jasao-server/src/main/kotlin/io/yeahx4/jasao/controller/UserController.kt
+++ jasao-server/src/main/kotlin/io/yeahx4/jasao/controller/UserController.kt
@@ -23,6 +23,7 @@ import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseCookie
 import org.springframework.web.bind.annotation.CookieValue
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -205,5 +206,45 @@ class UserController(
         this.logger.info("Successful JWT token refresh: refresh $refreshToken")
 
         return Res(HttpResponse("Ok", RefreshResDto(newToken)), HttpStatus.OK)
+    }
+
+    @GetMapping("/auth/logout")
+    @Transactional
+    fun logout(
+        @CookieValue refreshToken: String?,
+        @RequestHeader("Authorization") jwt: String,
+        response: HttpServletResponse
+    ): Res<String> {
+        if (refreshToken == null) {
+            this.logger.warn("Logout without refresh token. jwt: $jwt")
+            return Res(HttpResponse("Login First", null), HttpStatus.FORBIDDEN)
+        }
+        val pair = this.refreshTokenService.findRefreshJwtPair(refreshToken, jwt)
+
+        if (pair == null) {
+            this.logger.warn("Invalid refresh and jwt pair: ($refreshToken, $jwt)")
+
+            val cookie = ResponseCookie.from("refreshToken")
+                .maxAge(0)
+                .path("/")
+                .sameSite("None")
+                .httpOnly(true)
+                .build()
+            response.setHeader("Set-Cookie", cookie.toString())
+
+            return Res(HttpResponse("Invalid Token", null), HttpStatus.BAD_REQUEST)
+        }
+
+        val cookie = ResponseCookie.from("refreshToken")
+            .maxAge(0)
+            .path("/")
+            .sameSite("None")
+            .httpOnly(true)
+            .build()
+        response.setHeader("Set-Cookie", cookie.toString())
+        this.refreshTokenService.deleteByRefresh(refreshToken)
+
+        this.logger.info("Successful logout user ${pair.user}")
+        return Res(HttpResponse("Ok", null), HttpStatus.OK)
     }
 }
