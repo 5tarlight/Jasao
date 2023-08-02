1a15216f06764df002f2c87a8b29e0613e0d3eb2
YEAHx4 <developerstarlight@gmail.com>
2023-08-02 18:35:40 +0900

    remove debug log

--- jasao-frontend/src/pages/auth/Logout.tsx
+++ jasao-frontend/src/pages/auth/Logout.tsx
@@ -6,7 +6,6 @@ export default function Logout() {
   useEffect(() => {
     const storage = getStorage();
     if (!storage || !storage.login?.login) return;
-    console.dir(storage);
 
     request(
       "get",
