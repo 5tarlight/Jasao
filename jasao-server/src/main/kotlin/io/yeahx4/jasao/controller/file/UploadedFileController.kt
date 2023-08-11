19b6eabf24e9431e3c41eb48a37b59158079b105
YEAHx4 <developerstarlight@gmail.com>
2023-08-11 23:13:24 +0900

    remove unused import

--- jasao-server/src/main/kotlin/io/yeahx4/jasao/controller/file/UploadedFileController.kt
+++ jasao-server/src/main/kotlin/io/yeahx4/jasao/controller/file/UploadedFileController.kt
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
-import java.io.File
 
 @RestController
 @RequestMapping("/file")
--- jasao-server/src/main/kotlin/io/yeahx4/jasao/controller/user/UserController.kt
+++ jasao-server/src/main/kotlin/io/yeahx4/jasao/controller/user/UserController.kt
@@ -32,7 +32,6 @@ import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.multipart.MultipartFile
 
 @RestController
 @RequestMapping("/user")
