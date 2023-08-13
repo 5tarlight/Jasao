da1515e6bd4ea7181dcbcb99823230b02779135c
hellun205 <hellun515@gmail.com>
2023-08-13 19:03:45 +0900

    signup input validation
    implement #32

--- jasao-frontend/src/pages/auth/SignUp.tsx
+++ jasao-frontend/src/pages/auth/SignUp.tsx
@@ -15,6 +15,51 @@ export default function SignUp() {
   const navigate = useNavigate();
 
   const handleSubmit = () => {
+    const emailRe = new RegExp("^[\\w]+@([\\w-\\.]+\\.)+[\\w]{2,4}$");
+    const usernameRe = new RegExp("[\\w|ㄱ-ㅎ가-힣]");
+    const passwordRe = new RegExp(
+      "(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()\\-=])(?=.{8,})"
+    );
+
+    if (email.trim().length === 0) {
+      setWarn("이메일을 입력하세요.");
+      return;
+    } else if (!emailRe.test(email)) {
+      setWarn("이메일 형식이 아닙니다.");
+      return;
+    } else if (email.length > 100) {
+      setWarn("이메일이 너무 길어요.");
+      return;
+    }
+
+    if (username.trim().length === 0) {
+      setWarn("이름을 입력하세요.");
+      return;
+    } else if (
+      username.length < 2 ||
+      username.length > 20 ||
+      !usernameRe.test(username)
+    ) {
+      setWarn("이름은 2~20자의 영문 대/소문자, 한글, 숫자, _ 를 사용하세요.");
+      return;
+    }
+
+    if (password.trim().length === 0) {
+      setWarn("비밀번호를 입력하세요.");
+      return;
+    } else if (password.length < 8) {
+      setWarn("비밀번호는 8자 이상이어야 합니다.");
+      return;
+    } else if (password.length > 255) {
+      setWarn("비밀번호가 너무 길다.");
+      return;
+    } else if (!passwordRe.test(password)) {
+      setWarn(
+        "비밀번호는 영문 대/소문자, 숫자, 특수문자(!@#$%^&*()-=)를 포함해야 합니다."
+      );
+      return;
+    }
+
     if (password !== confirm) {
       setWarn("비밀번호가 일치하지 않습니다.");
       return;
