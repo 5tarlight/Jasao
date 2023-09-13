8e5bc632874a120aadbeb98796bc6e0e0ad655b3
YEAHx4 <developerstarlight@gmail.com>
2023-09-13 18:23:47 +0900

    util documentation

--- jasao-server/src/main/kotlin/io/yeahx4/jasao/util/Http.kt
+++ jasao-server/src/main/kotlin/io/yeahx4/jasao/util/Http.kt
@@ -2,6 +2,30 @@ package io.yeahx4.jasao.util
 
 import org.springframework.http.ResponseEntity
 
+/**
+ * Utility for consistent HTTP response format.
+ * `HttpResponse` data class will be converted into HTTP response body
+ * in JSON format like below:
+ * ```json
+ * {
+ *      "message": "OK",
+ *      "data": {
+ *          "username": "Jasao",
+ *          ...
+ *      }
+ * }
+ * ```
+ *
+ * This util is designed to use with `ResponseEntity`.
+ * When use this util with `ResponseEntity`, consider `Res`.
+ *
+ * @since 1.0.0
+ * @see Res
+ * @see ResponseEntity
+ * @param T Type of payload
+ * @param data payload of HTTP response
+ * @param message Message of HTTP response. Generally, coupled with HTTP code.
+ */
 data class HttpResponse<T>(
     val message: String,
     val data: T?
--- jasao-server/src/main/kotlin/io/yeahx4/jasao/util/unwrap.kt
+++ jasao-server/src/main/kotlin/io/yeahx4/jasao/util/unwrap.kt
@@ -2,6 +2,15 @@ package io.yeahx4.jasao.util
 
 import java.util.Optional
 
+/**
+ * Unwrap `Optional<T>` instance to `T?`.
+ * This util is developed for supporting compatibility between Kotlin and Java.
+ *
+ * @since 1.0.0
+ * @param optional instance of `Optional`
+ * @return Nullable `T`
+ * @see Optional
+ */
 fun <T> unwrap(optional: Optional<T>): T? {
     return if (optional.isPresent)
         optional.get()
