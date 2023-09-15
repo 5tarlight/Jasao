17716b95406f413935fa116878a24b259727743e
YEAHx4 <developerstarlight@gmail.com>
2023-09-16 00:46:13 +0900

    user service documentation

--- jasao-server/src/main/kotlin/io/yeahx4/jasao/service/user/RefreshTokenService.kt
+++ jasao-server/src/main/kotlin/io/yeahx4/jasao/service/user/RefreshTokenService.kt
@@ -4,30 +4,55 @@ import io.yeahx4.jasao.entity.user.RefreshToken
 import io.yeahx4.jasao.repository.user.RefreshTokenRepository
 import org.springframework.stereotype.Service
 
+/**
+ * Refresh token service required for JWT system.
+ *
+ * @since 1.0.0
+ */
 @Service
 class RefreshTokenService(private val refreshTokenRepository: RefreshTokenRepository) {
+    /**
+     * Check if refresh token is in use.
+     */
     fun isRefreshTokenDuplicated(token: String): Boolean {
         return this.refreshTokenRepository.findByRefresh(token) != null
     }
 
+    /**
+     * Save refresh token and JWT pair to DB
+     */
     fun saveRefreshToken(refresh: String, jwt: String, user: Long) {
         this.refreshTokenRepository.save(
             RefreshToken("", refresh, jwt, user, false)
         )
     }
 
+    /**
+     * Find refresh token
+     */
     fun findByRefresh(refresh: String): RefreshToken? {
         return this.refreshTokenRepository.findByRefresh(refresh)
     }
 
+    /**
+     * Find refresh token with refresh token and JWT pair
+     */
     fun findRefreshJwtPair(refresh: String, jwt: String): RefreshToken? {
         return this.refreshTokenRepository.findByRefreshAndJwt(refresh, jwt)
     }
 
+    /**
+     * Delete all refresh token of target user.
+     * This execution will cause every account of target user be logged out.
+     */
     fun deleteAllByUser(user: Long) {
         this.refreshTokenRepository.deleteAllByUser(user)
     }
 
+    /**
+     * Delete refresh token.
+     * This execution will cause login session with target refresh token be logged out.
+     */
     fun deleteByRefresh(refresh: String) {
         this.refreshTokenRepository.deleteByRefresh(refresh)
     }
